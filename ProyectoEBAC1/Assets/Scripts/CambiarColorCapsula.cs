978f72cd6c8cfa609720cf8816d70ec2b6b91473
PinguiGamer <msguepor@gmail.com>
2025-07-18 16:54:26 -0600

    Modulo 7 de Unity
    -Ediciones de color de los 3 GO previos mediante la implementación de scripts
    -Creación de 2 GO nuevos y la implementación de sus respectivos Scripts

--- ProyectoEBAC1/Assets/Scripts/CambiarColorCapsula.cs
+++ ProyectoEBAC1/Assets/Scripts/CambiarColorCapsula.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class VariablesGlobales
+{
+    public static bool CapsulaVariable = true;
+    public static bool CuboVariable = true;
+    public static bool EsferaVariable;
+    public static bool EsferaDuplicadaVariable = true;
+    public static bool CuboResultado3y4 = true;
+}
 public class CambiarColorCapsula : MonoBehaviour
 {
     public GameObject CapsulaPrefab;
     private void Awake()
     {
-        Color c = new Color(Random.value, Random.value, Random.value);
+        Color c = new Color(255, 255, 255);
         CapsulaPrefab.GetComponent<MeshRenderer>().material.color = c;
+        VariablesGlobales.CapsulaVariable = true;
+        Debug.Log(VariablesGlobales.CapsulaVariable);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,8 +28,19 @@ public class CambiarColorCapsula : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (VariablesGlobales.CapsulaVariable == true)
+        {
+            Color c = new Color(0, 0, 0);
+            GetComponent<MeshRenderer>().material.color = c;
+            VariablesGlobales.CapsulaVariable = false;
+        }
+        else
+        {
+            Color c = new Color(255, 255, 255);
+            GetComponent<MeshRenderer>().material.color = c;
+            VariablesGlobales.CapsulaVariable = true;
+        }
     }
 }
