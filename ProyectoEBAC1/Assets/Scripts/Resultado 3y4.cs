9f315a8d4345f18e3e5e9e78431b61dcb69c71cf
PinguiGamer <msguepor@gmail.com>
2025-07-18 22:32:13 -0600

    Corrección del Proyecto Modulo 7
    -Correcciones al código del Proyecto de Unity

---
+++ ProyectoEBAC1/Assets/Scripts/Resultado 3y4.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Resultado3y4 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        VariablesGlobales.CuboResultado3y4 = true;
+        Color c = new Color(250, 250, 250);
+        GetComponent<MeshRenderer>().material.color = c;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (VariablesGlobales.EsferaDuplicadaVariable || VariablesGlobales.EsferaVariable)
+        {
+            VariablesGlobales.CuboResultado3y4 = true;
+            Color c = new Color(250, 250, 250);
+            GetComponent<MeshRenderer>().material.color = c;
+            Debug.Log(VariablesGlobales.CuboResultado3y4);
+        }
+        else
+        {
+            VariablesGlobales.CuboResultado3y4 = false;
+            Color c = new Color(0, 0, 0);
+            GetComponent<MeshRenderer>().material.color = c;
+            Debug.Log(VariablesGlobales.CuboResultado3y4);
+        }
+    }
+}
